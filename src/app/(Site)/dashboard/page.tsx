3eb986a62d330952816e2a5245ddc401ab9b4ab8
waqqir humaid <waqqirhumaid1@gmail.com>
2025-12-28 23:23:38 +0700

    dash Responsive

--- src/app/(Site)/dashboard/page.tsx
+++ src/app/(Site)/dashboard/page.tsx
@@ -17,6 +17,8 @@ import api from "@/lib/axios";
 import { AxiosError } from "axios";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
+import TabelIzinBaru from "@/components/TabelIzinBaru";
+import TabelTugasGuru from "@/components/TabelTugasGuru";
 
 interface StudentPermit {
   id: number;
@@ -26,11 +28,11 @@ interface StudentPermit {
   hours_end: number;
   created_at: string;
 
-  student: {
+  students: {
     nis: number;
     name: string;
     class: string;
-  };
+  }[];
 }
 
 interface UserAuth {
@@ -41,11 +43,11 @@ interface UserAuth {
 }
 interface Permission {
   id: number;
-  student: {
+  students: {
     nis: number;
     name: string;
     class: string;
-  };
+  }[];
   mapel: {
     id: number;
     username: string;
@@ -75,31 +77,6 @@ interface Staff {
   day_name: string;
 }
 
-interface Assignment {
-  id: number;
-  assignment_details: string;
-  reason: string;
-  due_date: string;
-  created_at: string;
-  updated_at: string;
-
-  teacher: {
-    id: number;
-    username: string;
-    fullname: string;
-  };
-
-  class: {
-    id: number;
-    class: string;
-  };
-
-  subject: {
-    id: number;
-    name: string;
-  };
-}
-
 interface UserAuth {
   id: number;
   username: string;
@@ -111,9 +88,7 @@ export default function DashPage() {
   const router = useRouter();
 
   const [user, setUser] = useState<UserAuth>();
-  const [permissions, setPermissions] = useState<Permission[]>([]);
   const [piketStaff, setPiketStaff] = useState<Staff[]>([]);
-  const [assignments, setAssignments] = useState<Assignment[]>([]);
 
   const isAdmin = user?.roles?.includes("admin");
   const isPiket = user?.roles?.includes("piket");
@@ -171,15 +146,6 @@ export default function DashPage() {
     if (isMapel) fetchMapelPermits();
   }, [isMapel]);
 
-  const formatTanggalIndo = (dateString: string) => {
-    if (!dateString) return "-";
-    return new Date(dateString).toLocaleDateString("id-ID", {
-      day: "numeric",
-      month: "short",
-      hour: "2-digit",
-      minute: "2-digit",
-    });
-  };
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -196,20 +162,11 @@ export default function DashPage() {
           withCredentials: true,
         });
         setUser(authRes.data.data);
-        const [izinRes, piketRes, tugasRes] = await Promise.allSettled([
-          api.get("/student-permits"),
+        const [ piketRes] = await Promise.allSettled([
           api.get("/piket-schedules"),
-          api.get("/teacher-assignments"),
         ]);
-
-        if (izinRes.status === "fulfilled")
-          setPermissions(izinRes.value.data.data || []);
-
         if (piketRes.status === "fulfilled")
           setPiketStaff(piketRes.value.data.data || []);
-
-        if (tugasRes.status === "fulfilled")
-          setAssignments(tugasRes.value.data.data || []);
       } catch (error) {
         if (error instanceof AxiosError && error.response?.status === 401) {
           console.warn("Unauthorized, redirecting to login");
@@ -260,68 +217,7 @@ export default function DashPage() {
 
       {(isAdmin || isPiket) && (
         <>
-          <h2 className="md:text-xl text-md text-black/30 font-bold">
-            Laporan Izin Terbaru
-          </h2>
-          <div className="overflow-auto max-h-[270px]">
-            <Table className="bg-[#FFFFFF]/90 shadow-xl rounded-lg">
-              <TableHeader className="sticky z-10 bg-[#FFFFFF]/90 top-0">
-                <TableRow>
-                  <TableHead className="drop-shadow-2xl text-gray-400 font-bold text-xl">
-                    Waktu
-                  </TableHead>
-                  <TableHead className="drop-shadow-2xl text-gray-400 font-bold text-xl">
-                    NIS
-                  </TableHead>
-                  <TableHead className="drop-shadow-2xl text-gray-400 font-bold text-xl">
-                    Nama
-                  </TableHead>
-                  <TableHead className="drop-shadow-2xl text-gray-400 font-bold text-xl">
-                    Kelas
-                  </TableHead>
-                  <TableHead className="drop-shadow-2xl text-gray-400 font-bold text-xl">
-                    Alasan
-                  </TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {permissions.length > 0 ? (
-                  permissions.slice(0, 5).map((permission) => (
-                    <TableRow key={permission.id}>
-                      <TableCell className="text-gray-600 font-medium text-lg">
-                        {formatTanggalIndo(permission.created_at)}
-                      </TableCell>
-                      <TableCell className="text-gray-600 font-medium text-lg">
-                        {permission.student.nis}
-                      </TableCell>
-                      <TableCell className="text-gray-600 font-medium text-lg">
-                        {permission.student.name}
-                      </TableCell>
-                      <TableCell className="text-gray-600 font-medium text-lg">
-                        {permission.student.class}
-                      </TableCell>
-                      <TableCell className="text-gray-600 font-medium text-lg">
-                        {permission.reason}
-                      </TableCell>
-                    </TableRow>
-                  ))
-                ) : (
-                  <TableRow>
-                    <TableCell
-                      colSpan={5}
-                      className="text-center h-24 text-gray-500"
-                    >
-                      {loadingData ? (
-                        <Loader2 className="animate-spin mx-auto" />
-                      ) : (
-                        "Tidak ada data izin (atau backend offline)"
-                      )}
-                    </TableCell>
-                  </TableRow>
-                )}
-              </TableBody>
-            </Table>
-          </div>
+          <TabelIzinBaru />
 
           <div className="flex flex-col md:flex-row gap-5 mt-5 flex-1 min-h-0">
             <div className="flex flex-col md:w-1/3">
@@ -359,7 +255,7 @@ export default function DashPage() {
 
             <div className="space-y-5 flex md:flex-col flex-col md:space-y-5 flex-1 min-w-0">
               <div className="w-full h-32 bg-[#00786E]/60 shadow-md rounded-lg p-8 flex justify-center items-center ">
-                <p className="text-6xl md:text-8xl font-bold text-white">
+                <p className="sm:text-6xl text-4xl md:text-8xl font-bold text-white">
                   {formattedTime}
                 </p>
                 <div className="ml-5 md:ml-10 flex flex-col">
@@ -372,71 +268,7 @@ export default function DashPage() {
                 </div>
               </div>
 
-              <div className="bg-white/90 shadow-md rounded-lg p-1 flex-1 border justify-center items-center border-white/50 overflow-hidden min-w-0 ">
-                <ScrollArea className="w-full whitespace-nowrap h-full">
-                  <div className="flex w-max space-x-4 p-4 h-full">
-                    {assignments.length > 0 ? (
-                      assignments.map((note) => (
-                        <Card
-                          key={note.id}
-                          className="w-[300px] border-l-4 border-l-[#00786E] bg-black border-y-0 border-r-0 shadow-sm hover:shadow-md justify-center items-center  transition-all whitespace-normal"
-                        >
-                          <CardContent className="p-3 flex flex-col h-full justify-between">
-                            <div>
-                              <div className="flex justify-between items-start mb-2">
-                                <div>
-                                  <span className="bg-[#00786E]/60 text-white text-xs font-bold px-2 py-0.5 rounded mr-2">
-                                    {note.class?.class}
-                                  </span>
-                                </div>
-                                <span className="text-xs text-gray-400">
-                                  {formatTanggalIndo(note.created_at)}
-                                </span>
-                              </div>
-                              <div className="text-xs text-gray-400 font-semibold mt-1">
-                                Deadline: {formatTanggalIndo(note.due_date)}
-                              </div>
-
-                              <div className="flex space-x-4">
-                                <div className="flex items-center gap-2 mb-1">
-                                  <BookOpen
-                                    size={16}
-                                    className="text-[#007D72]"
-                                  />
-                                  <p className="font-bold text-gray-700 text-sm">
-                                    {note.subject?.name}
-                                  </p>
-                                </div>
-                              </div>
-                              <div className="flex items-center gap-2 mb-2">
-                                <User size={16} className="text-gray-400" />
-                                <p className="text-xs text-gray-500">
-                                  {note.teacher?.fullname}
-                                </p>
-                              </div>
-                            </div>
-                            <p className="text-sm text-gray-600 bg-white p-2 rounded border border-yellow-100 italic line-clamp-3">
-                              {note.assignment_details}
-                            </p>
-                          </CardContent>
-                        </Card>
-                      ))
-                    ) : (
-                      <div className="flex flex-col items-center justify-center w-full min-w-[300px] h-full text-gray-400">
-                        {loadingData ? (
-                          <Loader2 className="animate-spin mb-2" />
-                        ) : (
-                          <>
-                            <AlertCircle className="mb-2" />
-                            Belum ada tugas titipan
-                          </>
-                        )}
-                      </div>
-                    )}
-                  </div>
-                  <ScrollBar orientation="horizontal" />
-                </ScrollArea>
-              </div>
+              <TabelTugasGuru />
             </div>
           </div>
         </>
@@ -463,11 +295,12 @@ export default function DashPage() {
             <TableBody>
               {permits.map((permit) => (
                 <TableRow key={permit.id}>
-                  <TableCell>{permit.student.nis}</TableCell>
-                  <TableCell>{permit.student.name}</TableCell>
-                  <TableCell>{permit.student.class}</TableCell>
+                  {/* Gunakan optional chaining dan index [0] */}
+                  <TableCell>{permit.students?.[0]?.nis || "-"}</TableCell>
+                  <TableCell>{permit.students?.[0]?.name || "-"}</TableCell>
+                  <TableCell>{permit.students?.[0]?.class || "-"}</TableCell>
                   <TableCell>
-                    {permit.hours_start} - {permit.hours_end}
+                    {permit.hours_start} - {permit.hours_end || "Selesai"}
                   </TableCell>
                   <TableCell className="line-clamp-2">
                     {permit.reason}
---
+++ src/components/TabelTugasGuru.tsx
@@ -0,0 +1,136 @@
+"use client";
+import { AlertCircle, BookOpen, Loader2, User } from "lucide-react";
+import { Card, CardContent } from "./ui/card";
+import { ScrollArea, ScrollBar } from "./ui/scroll-area";
+import { useEffect, useState } from "react";
+import api from "@/lib/axios";
+
+interface Assignment {
+  id: number;
+  assignment_details: string;
+  reason: string;
+  due_date: string;
+  created_at: string;
+  updated_at: string;
+
+  teacher: {
+    id: number;
+    username: string;
+    fullname: string;
+  };
+
+  class: {
+    id: number;
+    class: string;
+  };
+
+  subject: {
+    id: number;
+    name: string;
+  };
+}
+
+export default function TabelTugasGuru() {
+    const [loadingData, setLoadingData] = useState(true);
+    const [assignments, setAssignments] = useState<Assignment[]>([]);
+
+    const fetchAssignments = async () => {
+        setLoadingData(true);
+        try {
+          const res = await api.get("/teacher-assignments", {
+            withCredentials: true,
+          });
+          setAssignments(res.data.data || []);
+        } catch (err) {
+          console.error(err);
+        } finally {
+          setLoadingData(false);
+        }
+      };
+
+    useEffect(() =>  {
+        fetchAssignments();
+    },[]);
+
+      const formatTanggalIndo = (dateString: string) => {
+        if (!dateString) return "-";
+        return new Date(dateString).toLocaleDateString("id-ID", {
+          day: "numeric",
+          month: "short",
+          hour: "2-digit",
+          minute: "2-digit",
+        });
+      };
+
+    return (
+        <>
+        <h2>
+        <span className="block sm:hidden text-xl font-bold text-gray-600 drop-shadow-2xl">
+          Tugas Guru
+        </span>
+        </h2>
+      <div className="bg-white/90 shadow-md rounded-lg p-1 flex-1 border justify-center items-center border-white/50 overflow-hidden min-w-0 ">
+        <ScrollArea className="w-full whitespace-nowrap h-full">
+          <div className="flex w-max space-x-4 p-2 h-full">
+            {assignments.length > 0 ? (
+              assignments.map((note) => (
+                <Card
+                  key={note.id}
+                  className="max-w-[300px] min-w-[225px] border-l-4 border-l-[#00786E] bg-black/5 border-y-0 border-r-0 shadow-sm hover:shadow-md justify-center items-center  transition-all whitespace-normal"
+                >
+                  <CardContent className="p-3 flex flex-col h-full justify-between">
+                    <div>
+                      <div className="flex justify-between items-start mb-2">
+                        <div>
+                          <span className="bg-[#00786E]/60 text-white text-xs font-bold px-2 py-0.5 rounded mr-2">
+                            {note.class?.class}
+                          </span>
+                        </div>
+                        <span className="text-xs text-gray-400">
+                          {formatTanggalIndo(note.created_at)}
+                        </span>
+                      </div>
+                      <div className="text-xs text-gray-400 font-semibold mt-1">
+                        Deadline: {formatTanggalIndo(note.due_date)}
+                      </div>
+
+                      <div className="flex space-x-4">
+                        <div className="flex items-center gap-2 mb-1">
+                          <BookOpen size={16} className="text-[#007D72]" />
+                          <p className="font-bold text-gray-700 text-sm">
+                            {note.subject?.name}
+                          </p>
+                        </div>
+                      </div>
+                      <div className="flex items-center gap-2 mb-2">
+                        <User size={16} className="text-gray-400" />
+                        <p className="text-xs text-gray-500">
+                          {note.teacher?.fullname}
+                        </p>
+                      </div>
+                    </div>
+                    <p className="text-sm text-gray-600 bg-white p-2 rounded border border-yellow-100 italic line-clamp-3">
+                      {note.assignment_details}
+                    </p>
+                  </CardContent>
+                </Card>
+              ))
+            ) : (
+              <div className="flex flex-col items-center justify-center w-full min-w-[300px] h-full text-gray-400">
+                {loadingData ? (
+                  <Loader2 className="animate-spin mb-2" />
+                ) : (
+                  <>
+                    <AlertCircle className="mb-2" />
+                    Belum ada tugas titipan
+                  </>
+                )}
+              </div>
+            )}
+          </div>
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
+      </div>
+        </>
+    );
+}
